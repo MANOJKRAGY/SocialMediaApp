58c6d55152ac1867681920eee35849712fda29ac
Manoj kumar <manojkragy@gmail.com>
2023-04-02 13:43:37 +0530

    inital commit for checkg the hard commit

--- src/main/SocialMediaApp/Application.java
+++ src/main/SocialMediaApp/Application.java
@@ -3,5 +3,6 @@ package main.SocialMediaApp;
 public class Application {
     public static void main(String[] args) {
     	//all the driver logic i would be writting here
+    	//thhis is to check the hard reset of the git i would doing the hard reset.
     }
 }
